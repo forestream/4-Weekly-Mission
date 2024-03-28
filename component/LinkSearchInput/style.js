ee3c25b64f5c477677a20ab16ea744243de6a965
hanbin <rj2303@naver.com>
2024-03-28 21:12:08 +0900

    nextjs 마이그레이션

---
+++ component/LinkSearchInput/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const LinkSearchInputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  background-color: #f5f5f5;
+  width: 100%;
+  max-width: 1080px;
+  margin: 40px auto;
+  padding: 16px 20px;
+  border-radius: 14px;
+
+  & input {
+    background: none;
+    width: 100%;
+    border: none;
+    font-size: 1rem;
+  }
+`;
+
+export const SearchIcon = styled.img`
+  width: 1rem;
+  height: 1rem;
+`;
+export const ClearIcon = styled.img`
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
+`;
