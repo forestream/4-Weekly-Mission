c1214b4150ee3bda551eab80fe5ef27c38df3b7b
hanbin <rj2303@naver.com>
2024-03-30 20:01:03 +0900

    페이지별 이동 링크 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -9,6 +10,9 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<Link href="/shared">/shared</Link>
+			<br />
+			<Link href="/folder">/folder</Link>
 		</>
 	);
 }
