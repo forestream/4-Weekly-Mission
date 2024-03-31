b4067e69e805b0498df52ddbbc5f77ff7c1a1fc8
hanbin <rj2303@naver.com>
2024-03-31 14:13:02 +0900

    검색 내용 지우기 이미지 크기 수정

--- component/LinkItems/index.tsx
+++ component/LinkItems/index.tsx
@@ -2,31 +2,30 @@ import { SharedFolderLink, LinkDatum, FolderListDatum } from "../../apis/api";
 import LinkItem from "./LinkItem";
 import styles from "./LinkItems.module.css";
 import LinkSkeleton from "./LinkSkeleton";
-import { LinkImageLoading, LinkLoading, LoadingAnimation } from "./style";
 
 interface Props {
-	folders?: FolderListDatum[];
-	links: (SharedFolderLink | LinkDatum)[];
-	isLoading?: boolean;
+  folders?: FolderListDatum[];
+  links: (SharedFolderLink | LinkDatum)[];
+  isLoading?: boolean;
 }
 
 const LinkItems = ({ folders, links = [], isLoading }: Props) => {
-	if (isLoading) {
-		return (
-			<div className={styles.LinkItems}>
-				<LinkSkeleton />
-			</div>
-		);
-	}
+  if (isLoading) {
+    return (
+      <div className={styles.LinkItems}>
+        <LinkSkeleton />
+      </div>
+    );
+  }
 
-	return (
-		<div className={styles.LinkItems}>
-			{links.map((link) => (
-				<LinkItem folders={folders} key={link.id} link={link} />
-			))}
-			{links.length !== 0 || <div>저장된 링크가 없습니다.</div>}
-		</div>
-	);
+  return (
+    <div className={styles.LinkItems}>
+      {links.map((link) => (
+        <LinkItem folders={folders} key={link.id} link={link} />
+      ))}
+      {links.length !== 0 || <div>저장된 링크가 없습니다.</div>}
+    </div>
+  );
 };
 
 export default LinkItems;
--- component/LinkSearchInput/style.js
+++ component/LinkSearchInput/style.js
@@ -1,28 +1,25 @@
 import styled from "styled-components";
 
 export const LinkSearchInputWrapper = styled.form`
-	display: flex;
-	align-items: center;
-	gap: 0.5rem;
-	background-color: #f5f5f5;
-	width: 100%;
-	max-width: 1080px;
-	margin: 40px auto;
-	padding: 16px 20px;
-	border-radius: 14px;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  background-color: #f5f5f5;
+  width: 100%;
+  max-width: 1080px;
+  margin: 40px auto;
+  padding: 16px 20px;
+  border-radius: 14px;
 
-	& input {
-		background: none;
-		width: 100%;
-		border: none;
-		font-size: 1rem;
-	}
+  & input {
+    background: none;
+    width: 100%;
+    border: none;
+    font-size: 1rem;
+  }
 `;
 
 export const ClearIcon = styled.div`
-	position: relative;
-	cursor: pointer;
-	width: 24px;
-	height: 24px;
-	flex-shrink: 0;
+  cursor: pointer;
+  flex-shrink: 0;
 `;
