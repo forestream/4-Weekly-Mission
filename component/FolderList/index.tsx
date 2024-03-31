d03f2ff1853240fa6a10383a766e4b7c6ba2780c
hanbin <rj2303@naver.com>
2024-03-31 13:47:53 +0900

    폴더 버튼 Link 컴포넌트 적용

--- component/FolderList/index.tsx
+++ component/FolderList/index.tsx
@@ -2,28 +2,27 @@ import { Ul, Container } from "./style";
 import AddFolder from "./AddFolder";
 import { FolderListDatum } from "../../apis/api";
 import { MouseEvent } from "react";
+import Link from "next/link";
 
 interface Props {
   folders: FolderListDatum[];
-  onClick: (e: MouseEvent<HTMLLIElement>) => void;
   selectedFolder: FolderListDatum;
 }
 
-const FolderList = ({ folders, onClick, selectedFolder }: Props) => {
-  const handleClick = (e: MouseEvent<HTMLLIElement>) => onClick(e);
-
+const FolderList = ({ folders, selectedFolder }: Props) => {
   return (
     <Container>
       <Ul>
         {folders.map((item) => (
-          <li
-            key={item.id}
-            id={item.id.toString()}
-            onClick={handleClick}
-            className={`${selectedFolder.id === item.id ? "selected" : ""}`}
-          >
-            {item.name}
-          </li>
+          <Link href={item.id === "ALL" ? "" : `?folderId=${item.id}`}>
+            <li
+              key={item.id}
+              id={item.id.toString()}
+              className={`${selectedFolder.id === item.id ? "selected" : ""}`}
+            >
+              {item.name}
+            </li>
+          </Link>
         ))}
       </Ul>
       <AddFolder />
--- pages/folder.tsx
+++ pages/folder.tsx
@@ -1,9 +1,9 @@
-import { MouseEvent, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
-	FolderListDatum,
-	getSavedFolderList,
-	getLinkData,
-	LinkDatum
+  FolderListDatum,
+  getSavedFolderList,
+  getLinkData,
+  LinkDatum,
 } from "@/apis/api";
 import LinkItems from "@/component/LinkItems";
 import { Container, FolderName } from "@/styles/Folder";
@@ -13,124 +13,110 @@ import BottomLinkAddInput from "@/component/BottomLinkAddInput";
 import FolderList from "@/component/FolderList";
 import LinkAddInput from "@/component/LinkAddInput";
 import LinkSearchInput from "@/component/LinkSearchInput";
-import { useRouter } from "next/router";
-import LinkSkeleton from "@/component/LinkItems/LinkSkeleton";
 
 const ALL: FolderListDatum = {
-	id: "ALL",
-	name: "전체",
-	favorite: false,
-	created_at: "",
-	link: {
-		count: 0
-	},
-	user_id: 0
+  id: "ALL",
+  name: "전체",
+  favorite: false,
+  created_at: "",
+  link: {
+    count: 0,
+  },
+  user_id: 0,
 };
 
 export async function getServerSideProps(context: any) {
-	const { folderId } = context.query;
-	const { data: folders } = await getSavedFolderList();
-	folders.unshift(ALL);
-	const { data: links } = await getLinkData(folderId);
+  let { folderId } = context.query;
+  folderId = folderId === undefined ? "ALL" : folderId;
+  const { data: folders } = await getSavedFolderList();
+  folders.unshift(ALL);
+  const { data: links } = await getLinkData(folderId);
 
-	return {
-		props: {
-			folders,
-			links
-		}
-	};
+  return {
+    props: {
+      folders,
+      links,
+      folderId,
+    },
+  };
 }
 
 interface Props {
-	folders: FolderListDatum[];
-	links: LinkDatum[];
+  folders: FolderListDatum[];
+  links: LinkDatum[];
+  folderId: string;
 }
 
-const FolderPage = ({ folders, links: initLinks }: Props) => {
-	const router = useRouter();
-	const [links, setLinks] = useState(initLinks);
-	const [selectedFolder, setSelectedFolder] = useState<FolderListDatum>(ALL);
-	const addLinkRef = useRef<HTMLDivElement>(null);
-	const footerRef = useRef<HTMLDivElement>(null);
-	const [addLinkIntersecting, setAddLinkIntersecting] = useState(false);
-	const [footerIntersecting, setFooterIntersecting] = useState(false);
+const FolderPage = ({ folders, folderId, links: initLinks }: Props) => {
+  const [links, setLinks] = useState(initLinks);
+  const addLinkRef = useRef<HTMLDivElement>(null);
+  const footerRef = useRef<HTMLDivElement>(null);
+  const [addLinkIntersecting, setAddLinkIntersecting] = useState(false);
+  const [footerIntersecting, setFooterIntersecting] = useState(false);
 
-	useEffect(() => {
-		setLinks(initLinks);
-	}, [initLinks]);
+  useEffect(() => {
+    setLinks(initLinks);
+  }, [initLinks]);
 
-	const handleClick = (e: MouseEvent<HTMLLIElement>) => {
-		const folderClicked = (e.target as HTMLLIElement).id;
-		const query = folderClicked === "ALL" ? "" : `?folderId=${folderClicked}`;
-		router.push(query);
+  const folderFound: any = folders.find((item) => String(item.id) === folderId);
 
-		const folderFound: any = folders.find(
-			(item) => String(item.id) === folderClicked
-		);
-		setSelectedFolder(folderFound);
-	};
+  const handleSearchSubmit = (keyword: string) => {
+    setLinks(
+      initLinks.filter(
+        (link) =>
+          link.url?.includes(keyword) ||
+          link.title?.includes(keyword) ||
+          link.description?.includes(keyword)
+      )
+    );
+  };
 
-	const handleSearchSubmit = (keyword: string) => {
-		setLinks(
-			initLinks.filter(
-				(link) =>
-					link.url?.includes(keyword) ||
-					link.title?.includes(keyword) ||
-					link.description?.includes(keyword)
-			)
-		);
-	};
+  // IntersectionObserver
+  const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      if (entry.target.id === "addLink")
+        setAddLinkIntersecting(entry.isIntersecting);
+      if (entry.target.id === "footer")
+        setFooterIntersecting(entry.isIntersecting);
+    });
+  };
 
-	// IntersectionObserver
-	const handleIntersect = (entries: IntersectionObserverEntry[]) => {
-		entries.forEach((entry) => {
-			if (entry.target.id === "addLink")
-				setAddLinkIntersecting(entry.isIntersecting);
-			if (entry.target.id === "footer")
-				setFooterIntersecting(entry.isIntersecting);
-		});
-	};
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersect, {
+      threshold: 0.4,
+    });
 
-	useEffect(() => {
-		const observer = new IntersectionObserver(handleIntersect, {
-			threshold: 0.4
-		});
+    if (addLinkRef.current && footerRef.current) {
+      observer.observe(addLinkRef.current);
+      observer.observe(footerRef.current);
+    }
 
-		if (addLinkRef.current && footerRef.current) {
-			observer.observe(addLinkRef.current);
-			observer.observe(footerRef.current);
-		}
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
-		return () => {
-			observer.disconnect();
-		};
-	}, []);
-
-	return (
-		<>
-			<div id="addLink" ref={addLinkRef}>
-				<LinkAddInput folders={folders} />
-			</div>
-			<Container>
-				<LinkSearchInput onSubmit={handleSearchSubmit} />
-				<FolderList
-					folders={folders}
-					selectedFolder={selectedFolder}
-					onClick={handleClick}
-				/>
-				<FolderName>
-					{selectedFolder.name}
-					<FolderOption selectedFolder={selectedFolder} />
-				</FolderName>
-				<LinkItems folders={folders} links={initLinks} isLoading={false} />
-				<MobileAddFolderButton />
-			</Container>
-			{!addLinkIntersecting && !footerIntersecting && (
-				<BottomLinkAddInput folders={folders} />
-			)}
-			<div id="footer" ref={footerRef}></div>
-		</>
-	);
+  return (
+    <>
+      <div id="addLink" ref={addLinkRef}>
+        <LinkAddInput folders={folders} />
+      </div>
+      <Container>
+        <LinkSearchInput onSubmit={handleSearchSubmit} />
+        <FolderList folders={folders} selectedFolder={folderFound} />
+        <FolderName>
+          {folderFound.name}
+          <FolderOption selectedFolder={folderFound} />
+        </FolderName>
+        <LinkItems folders={folders} links={links} isLoading={false} />
+        <MobileAddFolderButton />
+      </Container>
+      {!addLinkIntersecting && !footerIntersecting && (
+        <BottomLinkAddInput folders={folders} />
+      )}
+      <div id="footer" ref={footerRef}></div>
+    </>
+  );
 };
 
 export default FolderPage;
