fac728d19c3033d5e0bfeb69d0477e29b315698c
hanbin <rj2303@naver.com>
2024-04-05 22:17:34 +0900

    로그인 성공 시 페이지 전환 구현

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,10 +1,27 @@
 import Input from "@/component/Input";
 import checkEmailValidity from "@/utils/checkEmailValidity";
 import checkPasswordValidity from "@/utils/checkPasswordValidity";
+import axios from "@/lib/axios";
 import Image from "next/image";
 import Link from "next/link";
+import { FormEvent } from "react";
+import { useRouter } from "next/router";
 
 const Signin = () => {
+	const router = useRouter();
+
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const target = e.target as any;
+		const [email, password] = [target[0].value, target[1].value];
+		try {
+			const res = await axios.post("/sign-in", { email, password });
+			router.push("/folder");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<>
 			<Image
@@ -17,7 +34,7 @@ const Signin = () => {
 				회원이 아니신가요?
 				<Link href="/signup">회원 가입하기</Link>
 			</p>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<Input
 					placeholder="이메일을 입력해주세요."
 					type="email"
@@ -28,6 +45,7 @@ const Signin = () => {
 					type="password"
 					checkValidity={checkPasswordValidity}
 				></Input>
+				<button>로그인</button>
 			</form>
 		</>
 	);
