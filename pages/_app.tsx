ade91e88a05388548e8ba3be9da6e18af1f1f2df
hanbin <rj2303@naver.com>
2024-04-06 16:12:26 +0900

    signin signup page nav, profile 컴포넌트 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,28 +3,13 @@ import NavBar from "@/component/NavBar";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import React from "react";
-import { Router } from "next/router";
+import { Router, useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-	// const [loading, setLoading] = React.useState(false);
-	// React.useEffect(() => {
-	// 	const start = () => {
-	// 		setLoading(true);
-	// 	};
-	// 	const end = () => {
-	// 		setLoading(false);
-	// 	};
-	// 	Router.events.on("routeChangeStart", start);
-	// 	Router.events.on("routeChangeComplete", end);
-	// 	Router.events.on("routeChangeError", end);
-	// 	return () => {
-	// 		Router.events.off("routeChangeStart", start);
-	// 		Router.events.off("routeChangeComplete", end);
-	// 		Router.events.off("routeChangeError", end);
-	// 	};
-	// }, []);
+	const router = useRouter();
 
-	// pageProps.loading = loading;
+	if (router.route === "/signin" || router.route === "/signup")
+		return <Component {...pageProps} />;
 
 	return (
 		<>
