552e1f6edbfe4b8d730b74705dd08da9e246be61
hanbin <rj2303@naver.com>
2024-05-04 15:19:45 +0900

    feat: ga 코드 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import React from "react";
 import { Router, useRouter } from "next/router";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const router = useRouter();
@@ -13,6 +14,21 @@ export default function App({ Component, pageProps }: AppProps) {
 
 	return (
 		<>
+			{/* <!-- Google tag (gtag.js) --> */}
+			<Script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-WX9TQCE9PE"
+			/>
+			<Script
+				id="ga"
+				dangerouslySetInnerHTML={{
+					__html: `window.dataLayer = window.dataLayer || [];
+									function gtag(){dataLayer.push(arguments);}
+						    	gtag('js', new Date());
+									gtag('config', 'G-WX9TQCE9PE');
+										`
+				}}
+			/>
 			<NavBar />
 			<Component {...pageProps} />
 			<Footer />
